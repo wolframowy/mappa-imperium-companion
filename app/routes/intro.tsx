6a282a80058568b46c54a9cc6bc197c929173a1e
kompanpawel <pawelmiskiewicz14@gmail.com>
2025-10-02 02:42:44 +0200

    Fixes after CR

--- app/routes/intro.tsx
+++ app/routes/intro.tsx
@@ -4,11 +4,11 @@ import basics from "app/assets/text/Basics.json"
 export default function Intro() {
   return (
     <div className="flex @md:flex-col flex-wrap px-5 py-6 gap-4 w-full h-full overflow-y-auto">
-      <Paragraph title={'Basics'} text={basics.Entry} main/>
-      <Paragraph title={'Setup'} text={basics.Setup} underline />
-      <Paragraph title={'Solo Play'} text={basics.SoloPlay.Text} underline boldText={basics.SoloPlay.Bold} />
-      <Paragraph title={'Multiplayer'} text={basics.Multiplayer} underline />
-      <Paragraph title={'Drawing'} text={basics.Drawing} underline />
+      <Paragraph title={basics.Entry.Title} text={basics.Entry.Text} main/>
+      <Paragraph title={basics.Setup.Title} text={basics.Setup.Text} underline />
+      <Paragraph title={basics.SoloPlay.Title} text={basics.SoloPlay.Text} underline bold />
+      <Paragraph title={basics.Multiplayer.Title} text={basics.Multiplayer.Text} underline />
+      <Paragraph title={basics.Drawing.Title} text={basics.Drawing.Text} underline />
     </div>
   );
 }
