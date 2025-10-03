6c6598107c7075f7d165d9e720a1eeeebc8f3903
wolframowy <wolframowy@o2.pl>
2025-10-03 03:11:47 +0200

    Reorganize Era sections within a grid layout for improved structure

--- app/routes/playOverview.tsx
+++ app/routes/playOverview.tsx
@@ -9,30 +9,32 @@ export default function playOverview() {
       <Section title={json.Intro.Title} noUnderline main>
         <Paragraph textHtml={json.Intro.Text} />
       </Section>
-      {/*Era 1*/}
-      <Section title={json.Era1.Title}>
-        <Paragraph textHtml={json.Era1.Text} />
-      </Section>
-      {/*Era 2*/}
-      <Section title={json.Era2.Title}>
-        <Paragraph textHtml={json.Era2.Text} />
-      </Section>
-      {/*Era 3*/}
-      <Section title={json.Era3.Title}>
-        <Paragraph textHtml={json.Era3.Text} />
-      </Section>
-      {/*Era 4*/}
-      <Section title={json.Era4.Title}>
-        <Paragraph textHtml={json.Era4.Text} />
-      </Section>
-      {/*Era 5*/}
-      <Section title={json.Era5.Title}>
-        <Paragraph textHtml={json.Era5.Text} />
-      </Section>
-      {/*Era 6*/}
-      <Section title={json.Era6.Title}>
-        <Paragraph textHtml={json.Era6.Text} />
-      </Section>
+      <div className="grid lg:grid-rows-3 lg:grid-flow-col gap-4">
+        {/*Era 1*/}
+        <Section title={json.Era1.Title}>
+          <Paragraph textHtml={json.Era1.Text} />
+        </Section>
+        {/*Era 2*/}
+        <Section title={json.Era2.Title}>
+          <Paragraph textHtml={json.Era2.Text} />
+        </Section>
+        {/*Era 3*/}
+        <Section title={json.Era3.Title}>
+          <Paragraph textHtml={json.Era3.Text} />
+        </Section>
+        {/*Era 4*/}
+        <Section title={json.Era4.Title}>
+          <Paragraph textHtml={json.Era4.Text} />
+        </Section>
+        {/*Era 5*/}
+        <Section title={json.Era5.Title}>
+          <Paragraph textHtml={json.Era5.Text} />
+        </Section>
+        {/*Era 6*/}
+        <Section title={json.Era6.Title}>
+          <Paragraph textHtml={json.Era6.Text} />
+        </Section>
+      </div>
       {/*Game Length*/}
       <Section title={json.GameLength.Title}>
         <Paragraph textHtml={json.GameLength.Text} />
