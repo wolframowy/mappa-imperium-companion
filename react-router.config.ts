c588a840a8e462317d90687d9e5475cadcb89918
wolframowy <wolframowy@o2.pl>
2026-01-14 22:44:44 +0100

    Set basename for production environment in React Router config

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+  basename: process.env.NODE_ENV === "production" ? "/mappa-imperium-companion" : undefined,
 } satisfies Config;
