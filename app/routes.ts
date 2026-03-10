8d1257e1c6ee4e62bc234c54db00885d791ae85f
wolframowy <wolframowy@o2.pl>
2026-03-10 15:32:22 +0100

    feat: create About section and integrate navigation links

--- app/routes.ts
+++ app/routes.ts
@@ -8,8 +8,9 @@ export default [
   route("era-1", "routes/era1.tsx"),
   route("era-2", "routes/era2.tsx"),
   route("era-3", "routes/era3.tsx"),
-  route('era-4', 'routes/era4.tsx'),
-  route('era-5', 'routes/era5.tsx'),
-  route('era-6', 'routes/era6.tsx'),
-  route('special-rules', 'routes/specialRules.tsx')
+  route("era-4", "routes/era4.tsx"),
+  route("era-5", "routes/era5.tsx"),
+  route("era-6", "routes/era6.tsx"),
+  route("special-rules", "routes/specialRules.tsx"),
+  route("about", "routes/about.tsx"),
 ] satisfies RouteConfig;
