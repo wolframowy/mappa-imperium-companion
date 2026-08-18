8dbcbdd20e0d3e1ea8e1775b87523b96996e07f4
wolframowy <wolframowy@o2.pl>
2026-08-19 01:12:44 +0200

    Update base paths and favicon reference for consistency across the application

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,6 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  basename:
-    process.env.NODE_ENV === "production" ? "/mappa-imperium-companion/" : "/",
+  basename: "/mappa-imperium-companion/",
 } satisfies Config;
